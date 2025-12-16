25af2aa277f4128a93a80bb4c9e95b4d191fb431
Kacper Żmuda <zmudakacper03@gmail.com>
2025-12-16 08:16:56 +0100

    Prettier changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<TotalClicks />
+			<Button />
+			<footer>
+				<p>World Clicker 🌍 collects all clicks from whole world.</p>
+			</footer>
+		</>
+	);
 }
 
-export default App
+function TotalClicks() {
+	// Total clicks muszą być GET z serwera
+	return (
+		<>
+			<p className="total-clicks">123,411,516</p>
+			<p>total clicks</p>
+		</>
+	);
+}
+
+function Button() {
+	const [count, setCount] = useState(0);
+	// useEffect() = localStorage(?)
+	// count musi być POST na serwer
+	const sth: number = Number(localStorage.getItem("clicked"));
+
+	useEffect(() => {
+		localStorage.setItem("clicked", String(count));
+	}, [count]);
+
+	return (
+		<>
+			<button onClick={() => setCount(count => count + 1)}>Click me!</button>
+			<p>You've clicked {sth} times</p>
+		</>
+	);
+}
+
+export default App;
