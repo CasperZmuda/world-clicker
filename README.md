23da70159fc6a55bdf7d25884c4774af257caa4b
Kacper Żmuda <zmudakacper03@gmail.com>
2025-12-18 16:35:12 +0100

    TSX & CSS changes

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # 🌍 World Clicker
 
 Pierwszy projekt w React; służy do nauki. Cel: znajomość useState(), pisanie w TSX, komponenty.
-
-<!--
-Dodać:
-- proces uruchamiania (npm install, npm run dev)
-- opis aplikacji
-- wszystko po angielsku
--->
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,28 @@
-// PRZYSTOSOWAĆ POD TYPESCRIPT (TYPY, GENERICS, INTERFACE)
-// DODAĆ FOOTER
-
 import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [count, setCount] = useState<number>(0);
+
 	return (
 		<div className="content">
-			<TotalClicks />
-			<Button />
-		</div>
-	);
-}
+			<div className="total-clicks">
+				<p className="counter">{String(count)}</p>
+				<p className="text">total clicks</p>
+			</div>
 
-function TotalClicks() {
-	// Total clicks muszą być GET z serwera
-	return (
-		<div className="total-clicks">
-			<p className="counter">123,411,516</p>
-			<p className="text">total clicks</p>
-		</div>
-	);
-}
+			{/* <button onClick={() => setCount(count => count + 1)}>Click me!</button> */}
+			<button onClick={() => setCount(count + 1)}>Click me!</button>
 
-function Button() {
-	const [count, setCount] = useState(0);
-	// zrobić zapis w localstorage
-
-	return (
-		<>
-			<button onClick={() => setCount(count => count + 1)}>Click me!</button>
 			<p className="personal-clicks">
 				You've clicked <span>{count}</span> times
 			</p>
-		</>
+
+			<footer>
+				<p>World Clicker 🌍 collects all clicks from whole world.</p>
+			</footer>
+		</div>
 	);
 }
-
-// function Footer() {
-// 	return (
-// 		<footer>
-// 			<p>World Clicker 🌍 collects all clicks from whole world.</p>
-// 		</footer>
-// 	);
-// }
 
 export default App;
